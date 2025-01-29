c46f71dfe6a3f7b52ff6edfe0acffcde64c8f5aa
EclecticFella <hushlyadmin@protonmail.com>
2025-01-29 12:34:10 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,10 +2,5 @@ const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
   reactStrictMode: true,
-  basePath: isProd ? '/HUSH' : '',
-  assetPrefix: isProd ? '/HUSH/' : '',
-  images: {
-    unoptimized: true
-  },
   trailingSlash: true,
 };
